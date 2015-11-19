ce9f4e8ebb143f619e63a2acb4fc989b741bb42f
nedward <nedward777@gmail.com>
2015-11-18 20:00:50 -0800

    added jenkins basic

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+include_recipe "apt"
+include_recipe "jenkins"
+
+service 'jenkins' do
+  supports :restart => true
+  action [ :enable, :start ]
+end
+
+jenkins_plugin 'git' do
+  notifies :restart, 'service[jenkins]', :immediately
+end
